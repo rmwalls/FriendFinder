88106b4c31f5ef4f27eda6523d073befe9de7b2f
ZelleM <groovyrlm@gmail.com>
2019-07-17 20:25:41 -0400

    Pals constructor

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,14 @@
+//Set up array of pals
+var pals = [];
 
+function Pal(name, photo, scores) {
+  this.name = name;
+  this.photo = photo;
+  this.scores = scores;
+}
 
-const pals = [
-{
-    "name":"Lucy",
-    "photo":"https://www.deviantart.com/minionfan1024/art/Lucy-Van-Pelt-dancing-785086141",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }
-]
+pals.push(new Pal("Lucy", "photos/Lucy_van_Pelt.png", [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]));
+pals.push(new Pal("Charlie Brown", "photos/Charlie_Brown.png", [3, 1, 3, 2, 5, 1, 2, 5, 3, 3]));
+pals.push(new Pal("Snoopy", "photos/Snoopy.jpg", [5, 5, 3, 3, 5, 5, 4, 5, 2, 5]));
+
+module.exports = pals;
